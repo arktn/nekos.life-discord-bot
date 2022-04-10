3e3ab58bbd6edadd4c76c196ce45d052ec3ee253
fez <fektbox@gmail.com>
2022-04-10 20:09:36 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,24 +8,24 @@ module.exports = {
             message.channel.send('Check your DMs!');
             message.delete();
             const embed = new Discord.MessageEmbed()
-            .setAuthor(client.user.username, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }), 'https://github.com/ghaku/nekos.life-discord-bot')
+            .setAuthor(client.user.username, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }), 'https://github.com/fez6/nekos.life-discord-bot')
             .setThumbnail(client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
             .setTitle('Help')
             .setColor('7289da')
-            .setDescription(`You want to create a discord bot like this yourself?\n[Check out our Github page!](https://github.com/ghaku/nekos.life-discord-bot)`)
+            .setDescription(`You want to create a discord bot like this yourself?\n[Check out our Github page!](https://github.com/fez6/nekos.life-discord-bot)`)
             .addField('List of commands', prefix + 'commands')
-            .setFooter('Made with 💖 and discord.js by ghaku', client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+            .setFooter('Made with 💖 and discord.js by fez', client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
             message.author.send(embed);
             }
             if (!message.guild) {
               const embed = new Discord.MessageEmbed()
-              .setAuthor(client.user.username, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }), 'https://github.com/ghaku/nekos.life-discord-bot')
+              .setAuthor(client.user.username, client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }), 'https://github.com/fez6/nekos.life-discord-bot')
               .setThumbnail(client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
               .setTitle('Help')
               .setColor('7289da')
-              .setDescription(`You want to create a discord bot like this yourself?\n[Check out our Github page!](https://github.com/ghaku/nekos.life-discord-bot)`)
+              .setDescription(`You want to create a discord bot like this yourself?\n[Check out our Github page!](https://github.com/fez6/nekos.life-discord-bot)`)
               .addField('List of commands', prefix + 'commands')
-              .setFooter('Made with 💖 and discord.js by ghaku', client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
+              .setFooter('Made with 💖 and discord.js by fez', client.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
               message.author.send(embed);
             }
     }
